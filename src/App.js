be677c1c750564bc05220e5d75a424e4c27de3e1
Plouzin Zoé <plouzinzoe@gmail.com>
2021-12-16 17:32:55 +0100

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import UpdateStudent from './components/UpdateStudent';
+import AddStudent from './components/AddStudent';
+import {Routes, Route} from "react-router-dom";
+
+
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="" element={<Home></Home>} />
+        <Route path="add" element={<AddStudent></AddStudent>} />
+        <Route path="update" element={<UpdateStudent></UpdateStudent>} />
+      </Routes>
     </div>
   );
 }
