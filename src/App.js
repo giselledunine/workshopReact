1ec5d7e2870f91311a47dd0287fe254b18dde5f2
Plouzin Zoé <plouzinzoe@gmail.com>
2021-12-16 18:05:39 +0100

    functionnal routes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import './App.css';
 import Home from './components/Home';
 import UpdateStudent from './components/UpdateStudent';
 import AddStudent from './components/AddStudent';
-import {Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
 
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home></Home>} />
-        <Route path="" element={<Home></Home>} />
-        <Route path="add" element={<AddStudent></AddStudent>} />
-        <Route path="update" element={<UpdateStudent></UpdateStudent>} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/update" element={<UpdateStudent />} />
+          <Route path="/add" element={<AddStudent />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
