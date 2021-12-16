05e4b28f3977153be4322914af3ad08cb1d988a1
Plouzin Zoé <plouzinzoe@gmail.com>
2021-12-16 22:10:49 +0100

    Debut css + navbar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import Home from './components/Home';
 import UpdateStudent from './components/UpdateStudent';
 import AddStudent from './components/AddStudent';
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
-
-
+import {Navbar, Nav} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
+      <Navbar>
+          <Navbar.Brand href="#home">DCPARIS</Navbar.Brand>
+          <Nav className="nav">
+            <Nav.Link href="#home" className="navlink">Home</Nav.Link>
+            <Nav.Link href="#features" className="navlink">Features</Nav.Link>
+            <Nav.Link href="#pricing" className="navlink">Pricing</Nav.Link>
+          </Nav>
+      </Navbar>   
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
